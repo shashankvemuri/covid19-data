660c898d3d6aef8341bdd1327a601fef6d7c2c8c
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-06-25 12:55:45 -0400

    Update state_testPositivityRate.py

--- state_testPositivityRate.py
+++ state_testPositivityRate.py
@@ -13,11 +13,16 @@ daily_state = daily_state.loc[:, ['date', 'state', 'positive', 'negative', 'deat
 daily_state['testPositivityRate'] = round(daily_state['positive'] * 100 / daily_state['totalTestResults'], 2)
 daily_state['difference'] = round(daily_state['testPositivityRate'].pct_change(), 2)
 
-# val = input('Enter a state: ')
-# my_list = ['{}'.format(val)]
-my_list = ['NJ']
+val = str(input('Enter a state: '))
+val = val.upper()
+val = val.strip()
+my_list = ['{}'.format(val)]
+
 
 if my_list[0] in daily_state['state'].tolist():
+    states = pd.read_csv('state_codes.csv', index_col=0)
+    states = states.drop(columns = ['Abbrev'])
+
     daily_state = daily_state.set_index('state')
     daily_state = daily_state[daily_state.index.isin(my_list)]
     daily_state = daily_state.reset_index()
@@ -30,9 +35,11 @@ if my_list[0] in daily_state['state'].tolist():
     daily_state.columns = ['pos', 'neg', 'death', 'totalTests', 'testPositivityRate', 'diff']
     print ('\n', daily_state.head())
     
+    plt.gcf()
     rcParams['figure.figsize'] = 14, 7
     plt.plot(daily_state['testPositivityRate'])
-    plt.title(f'Test Positivity Rate for {my_list[0].upper()}')
+    state_name = states[states['Code']==f'{val}'].index.values
+    plt.title(f'Test Positivity Rate for {state_name[0]}')
     plt.xlabel('Date')
     plt.ylabel('Test Positivity Rate in %')
     plt.show()
